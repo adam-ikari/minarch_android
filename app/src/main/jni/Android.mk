b2e52e2cd50f9cfdbce32d0582d8477f5da2fbe1
Adam <chenzd123456@gmail.com>
2026-03-26 22:46:09 +0800

    Remove SDL2 dependency, fix lint errors
    - Remove SDL2 from Android.mk (not needed)
    - Disable AppLinkUrlError lint check
    - Build should work without SDL2 prebuilt

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,24 +1,19 @@
-# Android.mk for MinArch - With SDL2 dependency
+# Android.mk for MinArch
 MY_LOCAL_PATH := $(call my-dir)
 
-# Include SDL2 build from its own Android.mk
-include $(MY_LOCAL_PATH)/SDL2/Android.mk
-
-# MinArch core - with SDL2
+# MinArch core
 include $(CLEAR_VARS)
 LOCAL_MODULE := minarch
 LOCAL_C_INCLUDES := $(MY_LOCAL_PATH) \
                     $(MY_LOCAL_PATH)/minarch \
                     $(MY_LOCAL_PATH)/common \
-                    $(MY_LOCAL_PATH)/platform \
-                    $(MY_LOCAL_PATH)/SDL2/include
+                    $(MY_LOCAL_PATH)/platform
 
 LOCAL_SRC_FILES := $(MY_LOCAL_PATH)/minarch/jni_entry.c \
                    $(MY_LOCAL_PATH)/minarch/minarch_android.c \
                    $(MY_LOCAL_PATH)/platform/platform_android.c
 
 LOCAL_CFLAGS := -DPLATFORM=\"android\" \
-                -DUSE_SDL2 \
                 -DANDROID \
                 -fomit-frame-pointer \
                 -Ofast \
@@ -28,7 +23,5 @@ LOCAL_CFLAGS := -DPLATFORM=\"android\" \
 
 LOCAL_LDFLAGS := -ldl -lz -lm -llog
 LOCAL_LDLIBS := -lEGL -landroid
-LOCAL_SHARED_LIBRARIES := SDL2
-LOCAL_STATIC_LIBRARIES := SDL2_main
 
 include $(BUILD_SHARED_LIBRARY)
